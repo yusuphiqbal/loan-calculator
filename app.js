f0a142b78747a07b5d934b67a47c282c4f00f66c
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2020-04-07 18:28:12 +0300

    Added a function to calculate the results

---
+++ app.js
@@ -0,0 +1,25 @@
+document.querySelector('#loan-form').addEventListener('submit', calculateResults);
+
+function calculateResults(e) {
+  const amount = document.querySelector('#amount');
+  const interest = document.querySelector('#interest');
+  const years = document.querySelector('#years');
+  const monthlyPayment = document.querySelector('#monthly-payment');
+  const totalPayment = document.querySelector('#total-payment');
+  const totalInterst = document.querySelector('#total-interest');
+  const principal = parseFloat(amount.value);
+  const calculatedInterest = parseFloat(interest.value) / 100 / 12;
+  const calculatedPayments = parseFloat(years.value) * 12;
+  const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+  const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+  if (isFinite(monthly)) {
+    monthlyPayment.value = monthly.toFixed(2);
+    totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+    totalInterst.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+  } else {
+    console.log('Please check your numbers');
+  }
+
+  e.preventDefault();
+}
